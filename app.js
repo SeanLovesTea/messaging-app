9e5ad01dfe07b7a21cb970e9d5101ecff07eeced
SeanLovesTea <sean.m.concannon@gmail.com>
2023-08-01 12:30:02 +0100

    added partials and models

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const expressLayouts = require('express-ejs-layouts')
 const connectDb = require('./config/mongodb')
+const path = require('path')
 
 const app = express()
 
@@ -10,6 +11,8 @@ connectDb()
 
 app.use(expressLayouts)
 app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.static(path.join(__dirname + '/public')))
 
 app.get('/', (req, res) => {
   res.render('index')
