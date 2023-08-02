503ca37903c59634b07bc58597740ea15e12048c
SeanLovesTea <sean.m.concannon@gmail.com>
2023-08-02 11:54:30 +0100

    basic log in log out with passport added

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const path = require('path')
 const session = require('express-session')
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
+const User = require('./models/User')
 
 
 const app = express()
@@ -20,10 +21,44 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname + '/public')))
 
 app.use(session({ secret: "cats", resave: false, saveUninitialized: true }))
+app.use(express.urlencoded({ extended: false}))
 app.use(passport.initialize())
 app.use(passport.session())
-app.use(express.urlencoded({ extended: false}))
 
+passport.use(
+  new LocalStrategy(async(username, password, done) => {
+    try {
+      const user = await User.findOne({ username: username });
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" })
+      }
+      if (user.password !== password) {
+        return done(null, false, { message: "Incorrect password" })
+      }
+      return done(null, user)
+    } catch(err) {
+      return done(err)
+    }
+  })
+);
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+})
+
+passport.deserializeUser(async function(id, done) {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch(err) {
+    done(err)
+  }
+})
+
+app.use(function(req, res, next) {
+  res.locals.currentUser = req.user
+  next()
+})
 app.use('/', mainRoutes)
 
 app.listen(5000, () => console.log('App running on port 5000'))
--- routes/route.js
+++ routes/route.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/User')
+const passport = require('passport')
 
 router.get('/', (req, res) => {
+  // res.render('index', { user: req.user })
   res.render('index')
 })
+
 router.get('/sign-up', (req, res) => {
   res.render('sign-up')
 })
+
 router.post('/sign-up', async (req, res, next) => {
   try {
     const user = new User({
@@ -20,7 +24,17 @@ router.post('/sign-up', async (req, res, next) => {
     return next(error)
   }
 })
-// router.post('/', (req, res) => {
+router.post('/log-in', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/'
+}))
 
-// })
+router.get('/log-out', (req, res, next) => {
+  req.logout(function (err) {
+    if (err) {
+      return next(err)
+    }
+    res.redirect('/')
+  }) 
+})
 module.exports = router;
